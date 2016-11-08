4bd0ee36af1230942a62e5ff1b619a8373735ca5
Anders W. Olsen <awo@defalt.net>
2016-11-08 15:05:40 +0100

    Adding test files

---
+++ src/App.java
@@ -0,0 +1,12 @@
+/**
+ * Created by awo on 08/11/16.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("This works.");
+    }
+
+    public static boolean testBool() {
+        return true;
+    }
+}
