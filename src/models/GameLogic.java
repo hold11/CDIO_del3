296941fa8156abf2c6fa8178948d59efde2bf680
trules <troelsjc@gmail.com>
2016-11-23 13:59:38 +0100

    GameLogic draft

--- src/models/GameLogic.java
+++ src/models/GameLogic.java
@@ -1,7 +1,19 @@
 package models;
 
+import java.util.Collection;
+
 /**
  * Created by tjc on 23/11/16.
  */
-public class GameLogic {
+public class GameLogic
+{
+    private DiceCup diceCup;
+    private int playerTurn = 1;
+    private Collection<Player> players = Player.getPlayersList();
+
+    public GameLogic()
+    {
+        DiceCup = new diceCup();    // Creates the default dicecup with 2 dice with 6 sides.
+
+    }
 }
