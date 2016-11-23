61a043c755d56cf8be08a00d0864efc3fd413fd8
trules <troelsjc@gmail.com>
2016-11-23 14:17:25 +0100

    Created method skel in GameLogic using Design Class Diagram

--- src/models/GameLogic.java
+++ src/models/GameLogic.java
@@ -16,4 +16,65 @@ public class GameLogic
         DiceCup = new diceCup();    // Creates the default dicecup with 2 dice with 6 sides.
 
     }
+
+    /**
+     * playTurn
+     */
+    public void playTurn(Player)
+    {
+
+    }
+
+    /**
+     * hasWon checks if player has won.
+     * @return
+     */
+    public boolean hasWon(Player)
+    {
+
+    }
+
+    /**
+     * nextPlayer
+     */
+    public void nextPlayer()
+    {
+
+    }
+
+    /**
+     * getPlayerTurn
+     * @return
+     */
+    public int getPlayerTurn()
+    {
+
+    }
+
+    /**
+     * getCurrentPlayer
+     * @return
+     */
+    public Player getCurrentPlayer()
+    {
+
+    }
+
+    /**
+     * getDiceCup
+     * @return
+     */
+    public DiceCup getDiceCup()
+    {
+
+    }
+
+    /**
+     * getTotalEyes returns the total number of eyes of the rolled dice.
+     * @return
+     */
+    public int getTotalEyes(DiceCup)
+    {
+
+    }
 }
