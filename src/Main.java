176bc45ee7d051198af947f61d4f65661d7d4dbb
trules <troelsjc@gmail.com>
2016-11-23 17:22:31 +0100

    Removed testBool method in Main.java and pushed some brackets.

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,8 @@
 
 public class Main
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("This works.");
-    }
-
-    public static boolean testBool() {
-        return true;
     }
 }
