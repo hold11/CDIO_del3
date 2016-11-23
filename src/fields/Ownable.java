25af0d132bcdf7c5c38ad2d2479a4d2684585c7c
trules <troelsjc@gmail.com>
2016-11-23 14:03:57 +0100

    created reset method in Ownable.java class

--- src/fields/Ownable.java
+++ src/fields/Ownable.java
@@ -45,4 +45,9 @@ public abstract class Ownable extends Field
         this.owner = player;                                // Sets fields owner to player.
         player.getPlayerAccount().withdraw(this.price);     // Withdraws field price from player account
     }
+
+    public static void reset()
+    {
+        ownedOwnables.clear();
+    }
 }
