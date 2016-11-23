d4243cfdfe22961699e4728f739fb8b4e4ab08d7
trules <troelsjc@gmail.com>
2016-11-23 17:24:48 +0100

    Removed testBool test in MainTest.java

--- tests/MainTest.java
+++ tests/MainTest.java
@@ -17,10 +17,4 @@ public class MainTest {
     public void tearDown() throws Exception {
 
     }
-
-    @Test
-    public void testBool() throws Exception {
-        assertTrue(Main.testBool());
-    }
-
 }
